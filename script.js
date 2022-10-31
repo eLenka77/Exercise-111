4defbcdac9349d1f9f3cf427bea9ae0497670068
eLenka <lesic1990.06.05@gmail.com>
2022-10-31 17:24:31 +0100

    Task 111

--- script.js
+++ script.js
@@ -1,10 +1,42 @@
 function checkResult1() {
-  let value1 = document.getElementById("t1").value;
-  let value2 = document.getElementById("t2").value;
+  let value1 = Number(document.getElementById("t1").value);
+  let value2 = Number(document.getElementById("t2").value);
   let result = document.getElementById("r1");
   let arr = [];
   for (let i = value1; i <= value2; i++) {
     arr.push(i);
   }
   result.innerHTML = arr;
+}
+
+function checkResult2() {
+  let value1 = Number(document.getElementById("t1").value);
+  let value2 = Number(document.getElementById("t2").value);
+  let result = document.getElementById("r1");
+  let arr = [];
+  let arr2 = [];
+
+  for (let i = value1; i <= value2; i++) {
+    total = arr.push(i);
+    if (total % 2 === 0) {
+      arr2.push(total);
+    }
+    result.innerHTML = arr2;
+  }
+}
+
+function checkResult3() {
+  let value1 = Number(document.getElementById("t1").value);
+  let value2 = Number(document.getElementById("t2").value);
+  let result = document.getElementById("r1");
+  let arr = [];
+  let arr2 = [];
+
+  for (let i = value1; i <= value2; i++) {
+    total = arr.push(i);
+    if (total % 2 !== 0) {
+      arr2.push(total);
+    }
+    result.innerHTML = arr2;
+  }
 }
