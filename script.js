9ad4a01815d7d582179e757204f8c9b4e99208b7
eLenka <lesic1990.06.05@gmail.com>
2022-11-02 21:13:09 +0100

    Exercise 120

--- script.js
+++ script.js
@@ -86,11 +86,6 @@ function checkResult8() {
   }
 }
 
-window.onload = function () {
-  let codeBox1 = document.getElementById("code1");
-  codeBox1.innerHTML = checkResult8.toString();
-};
-
 function checkResult9() {
   let value1 = Number(document.getElementById("t9").value);
   let result = document.getElementById("r9");
@@ -162,3 +157,155 @@ function checkResult14() {
   }
   result.innerHTML = arr;
 }
+
+function checkResult15() {
+  let result = document.getElementById("r16");
+  let arr = [2, 5, 9, 3, 1, 4];
+  let res = 0;
+  for (let elem of arr) {
+    res += elem;
+  }
+  result.innerHTML = res;
+}
+
+function checkResult16() {
+  let result = document.getElementById("r16");
+  let arr = [2, 5, 9, 3, 1, 4];
+  let res = 0;
+  for (let elem of arr) {
+    if (elem % 2 === 0) {
+      res += elem;
+    }
+    result.innerHTML = res;
+  }
+}
+
+function checkResult17() {
+  let result = document.getElementById("r16");
+  let arr = [2, 5, 9, 3, 1, 4];
+  let res = 0;
+  for (let elem of arr) {
+    res += elem ** 2;
+  }
+  result.innerHTML = res;
+}
+
+function checkResult18() {
+  let result = document.getElementById("r16");
+  let arr = [2, 5, 9, 3, 1, 4];
+  let res = 1;
+  for (let elem of arr) {
+    res *= elem;
+  }
+  result.innerHTML = res;
+}
+
+function checkResult19() {
+  let result = document.getElementById("r19");
+  let str = "";
+  for (let i = 1; i <= 5; i++) {
+    str += "-";
+  }
+  result.innerHTML = str;
+}
+
+function checkResult20() {
+  let result = document.getElementById("r20");
+  let str = "";
+  for (let i = 1; i <= 9; i++) {
+    str += i;
+  }
+  result.innerHTML = str;
+}
+
+function checkResult21() {
+  let result = document.getElementById("r21");
+  let str = "";
+  for (let i = 9; i >= 1; i--) {
+    str += i;
+  }
+  result.innerHTML = str;
+}
+
+function checkResult22() {
+  let result = document.getElementById("r22");
+  let str = "-";
+  for (let i = 1; i <= 9; i++) {
+    str += i;
+    str += "-";
+  }
+  result.innerHTML = str;
+}
+
+function checkResult23() {
+  let result = document.getElementById("r23");
+  let arr = [];
+  for (let i = 10; i <= 1000; i++) {
+    let str = String(i);
+    arr += str[0];
+    result.innerHTML = arr;
+  }
+}
+
+function checkResult24() {
+  let result = document.getElementById("r24");
+  let arr = [];
+  for (let i = 10; i <= 1000; i++) {
+    let str = String(i);
+    arr.push(Number(str[0]) + Number(str[1]));
+    result.innerHTML = arr;
+  }
+}
+
+function checkResult25() {
+  let result = document.getElementById("r25");
+  let arr = [];
+  for (let i = 10; i <= 1000; i++) {
+    let str = String(i);
+    if (str[0] === "1") {
+      arr.push(i);
+    }
+    result.innerHTML = arr;
+  }
+}
+
+function checkResult26() {
+  let result = document.getElementById("r26");
+  let arr = [];
+  for (let i = 10; i <= 1000; i++) {
+    let str = String(i);
+    if (Number(str[0]) + Number(str[1]) == 5) {
+      arr.push(i);
+    }
+    result.innerHTML = arr;
+  }
+}
+
+window.onload = function () {
+  let codeBox1 = document.getElementById("code1");
+  codeBox1.innerHTML = checkResult8.toString();
+
+  let codeBox19 = document.getElementById("code19");
+  codeBox19.innerHTML = checkResult19.toString();
+
+  let codeBox20 = document.getElementById("code20");
+  codeBox20.innerHTML = checkResult20.toString();
+
+  let codeBox21 = document.getElementById("code21");
+  codeBox21.innerHTML = checkResult21.toString();
+
+  let codeBox22 = document.getElementById("code22");
+  codeBox22.innerHTML = checkResult22.toString();
+
+  let codeBox23 = document.getElementById("code23");
+  codeBox23.innerHTML = checkResult23.toString();
+
+  let codeBox24 = document.getElementById("code24");
+  codeBox24.innerHTML = checkResult24.toString();
+
+  let codeBox25 = document.getElementById("code25");
+  codeBox25.innerHTML = checkResult25.toString();
+
+  let codeBox26 = document.getElementById("code26");
+  codeBox26.innerHTML = checkResult26.toString();
+};
